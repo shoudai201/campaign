d331aab93cfc0f547fb4ee0c7fc4979900850f88
s-tanaka <>
2025-05-13 23:06:49 +0900

    利用日時直し

--- src/main/java/atm/UseDateTime.java
+++ src/main/java/atm/UseDateTime.java
@@ -1,20 +1,24 @@
 package atm;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
- * 利用日
+ * 利用日時
  * @param value
  */
 public record UseDateTime(LocalDateTime value) {
 
     public boolean isSaturdayOrSunday() {
-        return value.getDayOfWeek() == java.time.DayOfWeek.SATURDAY 
+        return value.getDayOfWeek() == java.time.DayOfWeek.SATURDAY
         || value.getDayOfWeek() == java.time.DayOfWeek.SUNDAY;
     }
 
     public boolean isWeekDay(){
         return !this.isSaturdayOrSunday();
+    }
 
+    public UseTime getUseTime(){
+        return new UseTime(this.value.toLocalTime());
     }
 }
