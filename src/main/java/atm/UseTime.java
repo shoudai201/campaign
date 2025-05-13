c421273b33fbac1360368c95f36f10bcc1fcf265
s-tanaka <>
2025-05-13 22:53:44 +0900

    利用日作成

---
+++ src/main/java/atm/UseTime.java
@@ -0,0 +1,17 @@
+package atm;
+
+import java.time.LocalTime;
+
+/**
+ * 利用日
+ */
+public record UseTime(LocalTime value) {
+
+    public boolean isEqualsAndBefore(LocalTime localTime){
+        return this.value.equals(localTime) || this.value.isBefore(localTime);
+    }
+
+    public boolean isEqualsAndAfter(LocalTime localTime){
+        return this.value.equals(localTime) || this.value.isAfter(localTime);
+    }
+}
