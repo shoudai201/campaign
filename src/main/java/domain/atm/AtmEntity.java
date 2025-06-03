ef4aea7b3f4038cd13f3724f113974ab2221f214
s-tanaka <>
2025-06-03 22:22:50 +0900

    feat: 手数料課金追加

--- src/main/java/domain/atm/AtmEntity.java
+++ src/main/java/domain/atm/AtmEntity.java
@@ -1,6 +1,5 @@
 package domain.atm;
 
-
 public class AtmEntity {
 
     private final AtmType atmType;
@@ -11,6 +10,26 @@ public class AtmEntity {
 
     
     public Fee chargeFee(UseDateTime useDateTime) {
-        return new Fee(100);
+        
+        if(this.atmType.equals(AtmType.銀行ATM)){
+
+            if(WorkingTime.銀行ATM.isWorkingTime(useDateTime.getUseTime())){
+                return new Fee(0);
+            }
+            return new Fee(110);
+            
+        }else if(this.atmType.equals(AtmType.コンビニATM)){
+            if(useDateTime.isSaturdayOrSunday()){
+                return new Fee(330);
+            }
+            if(useDateTime.isWeekDay()){
+                if(WorkingTime.コンビニATM.isWorkingTime(useDateTime.getUseTime())){
+                    return new Fee(220);
+                }
+                return new Fee(330);
+            }
+            throw new RuntimeException("コンビニATMの手数料が算出できません");
+        }
+        throw new RuntimeException("ATMの種類が特定できません");
     }
 }
