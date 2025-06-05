c346e7815d7f186e5f52723dd9b808efc116accd
s-tanaka <>
2025-06-05 23:18:25 +0900

    test: atmのテストを作成

---
+++ src/test/groovy/domain/atm/AtmEntitySpec.groovy
@@ -0,0 +1,30 @@
+package domain.atm
+
+import spock.lang.Specification
+import spock.lang.Unroll
+import java.time.LocalDateTime
+
+@Unroll
+class AtmEntitySpec extends Specification {
+
+    def "chargeFee_#usecase"() {
+        setup:
+        AtmEntity atmEntity = new AtmEntity(atmType)
+        UseDateTime useDateTime = new UseDateTime(dateTime)
+
+        when:
+        Fee fee = atmEntity.chargeFee(useDateTime)
+
+        then:
+        fee == expected
+
+        where:
+        usecase | atmType | dateTime                || expected
+        "銀行ATM営業時間内"|AtmType.銀行ATM | LocalDateTime.of(2023, 1, 6, 9, 0, 0)  || new Fee(0)
+        "銀行ATM営業時間外"|AtmType.銀行ATM | LocalDateTime.of(2023, 1, 6, 22, 0, 0) || new Fee(110)
+        "コンビニATM土日" | AtmType.コンビニATM | LocalDateTime.of(2023,1,7,22,0,0)|| new Fee(330)
+        "コンビニATM平日_営業時間内" | AtmType.コンビニATM | LocalDateTime.of(2023,1,6,9,0,0)|| new Fee(220)
+        "コンビニATM平日_営業時間外" | AtmType.コンビニATM | LocalDateTime.of(2023,1,6,21,0,0)|| new Fee(330)
+
+    }
+}
